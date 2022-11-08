b5f156b3bad9db8d54bd9a87a836ddb246546f2c
Veronica Axelsson <millernadragon@hotmail.com>
2022-11-08 19:02:57 +0100

    Buying info added

--- src/sections/BuyInfoSection.js
+++ src/sections/BuyInfoSection.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import divider from '../assets/images/icons/divider.svg'
+import icon1 from '../assets/images/icons/shopInfoIcons1.svg'
+import icon2 from '../assets/images/icons/shopInfoIcons2.svg'
+import icon3 from '../assets/images/icons/shopInfoIcons3.svg'
 
 const BuyInfoSection = () => {
   return (
     <>
-        {/* <div>BuyInfoSection</div> */}
+      <section className="buyInfoSection">
+        <img src={divider} className="divider" alt="showcase-img-1"/>
+
+        <div className="container ">
+          <div className="__flex ">
+
+            <div className="__1fr buyInfo">
+              <div className="circle">
+                <img src={icon1} alt="showcase-img-1"/>
+              </div>
+              <h3>Customer Support</h3>
+              <p>Village did removed enjoyed explain talking.</p>
+            </div>
+
+            <div className="__1fr buyInfo __marginL">
+              <div className="circle">
+                <img src={icon2} alt="showcase-img-1"/>
+              </div>
+              <h3>Secured Payment</h3>
+              <p>Village did removed enjoyed explain talking.</p>
+            </div>
+
+            <div className="__1fr buyInfo __marginR">
+              <div className="circle">
+                <img src={icon3} alt="showcase-img-1"/>
+              </div>
+              <h3>Free Home Delivery</h3>
+              <p>Village did removed enjoyed explain talking.</p>
+            </div>
+
+            <div className="__1fr buyInfo">
+              <div className="circle">
+                <img src={icon3} alt="showcase-img-1"/>
+              </div>
+              <h3>30 Day Reuters</h3>
+              <p>Village did removed enjoyed explain talking.</p>
+            </div>
+
+          </div>
+
+        </div>
+      </section>
     </>
   )
 }
